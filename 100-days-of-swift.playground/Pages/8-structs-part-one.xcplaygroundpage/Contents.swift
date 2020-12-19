b7bf39c1f86194b5462c7bc72f21e55149ff70d6
Misha Goncharov <forsomeletters@gmail.com>
2020-12-20 01:28:08 +0300

    Add a computed property to the previously defined Sport struct

--- 100-days-of-swift.playground/Pages/8-structs-part-one.xcplaygroundpage/Contents.swift
+++ 100-days-of-swift.playground/Pages/8-structs-part-one.xcplaygroundpage/Contents.swift
@@ -2,12 +2,20 @@ import Foundation
 
 struct Sport {
     var name: String
+    var isOlympic: Bool
+
+    // A computed property that depends on whether this is an Olympic sport or not
+    var olympicStatus: String {
+        return isOlympic ? "\(name) is an Olympic sport" : "\(name) is not an Olympic sport"
+    }
 }
 
-// Initializing a struct
-var tennis = Sport(name: "Tennis")
-print(tennis.name)
+// Creating an instance of an Olympic sport
+var tennis = Sport(name: "Tennis", isOlympic: true)
+print(tennis.olympicStatus)
 
-// Changing the name property of the struct
-tennis.name = "Lawn tennis"
-print(tennis.name)
+// Creating an object with a non-Olympic sport
+var chessboxing = Sport(name: "Chessboxing", isOlympic: false)
+print(chessboxing.olympicStatus)
+
+/* Behind the scenes, a computed property is effectively just a function call that happens to belong to a struct */
